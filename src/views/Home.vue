fe17cb5be5c8a1d6a41e4503abb78d9de032c56f
Guozhen <guozhen.hu@qq.com>
2020-07-22 23:46:07 +0800

    Carousel is added in home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="home">
-    <BaseNavBar></BaseNavBar>
+    <el-row :span="24" id="base-nav-bar">
+      <BaseNavBar></BaseNavBar>
+    </el-row>
+    <el-row :gutter="20" class="el-row-notnav">
+      <el-col :span="11" :offset="4">
+        <div>
+          <el-carousel height="270px">
+            <el-carousel-item>
+              <img
+                class="carousel-img"
+                src="../assets/oss/carousel_img/carousel_01.png"
+              />
+            </el-carousel-item>
+            <el-carousel-item>
+              <img
+                class="carousel-img"
+                src="../assets/oss/carousel_img/carousel_01.png"
+              />
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-col>
+      <el-col :span="5">
+        <div>
+          <img
+            class="carousel-img"
+            src="../assets/oss/carousel_img/carousel_01.png"
+          />
+        </div>
+      </el-col>
+    </el-row>
     <img src="../assets/logo.png" />
     <div>
       <p>
@@ -35,5 +65,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.el-row-notnav {
+  margin-top: 40px;
+}
+.carousel-img {
+  width: 100%;
+  height: auto;
 }
 </style>
